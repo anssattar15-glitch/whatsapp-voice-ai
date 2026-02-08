3924af5047c893b93ebf664ad60a920ebf6b7acc
anssattar15-glitch <anssattar15@gmail.com>
2026-02-08 22:05:54 +0500

    Create config.py

---
+++ app/app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+settings = Settings()
